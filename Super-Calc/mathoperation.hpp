739f231e82736d5b36762aee91c82503bb0b2f76
Tyopo131 <samecob62@gmail.com>
2025-01-16 15:50:11 +0000

    Improve MathTree and some stuff in MathOperations Add Numeric concept Improve AppendCalculation Improve Calculation Add "calculate" function to AppendCalculation and Calculation Add comments everywhere

--- Super-Calc/mathoperation.hpp
+++ Super-Calc/mathoperation.hpp
@@ -1,15 +1,29 @@
 #pragma once
+#include <concepts>
+
 namespace MathOperations {
+	template <typename T>
+	concept Numeric = std::integral<T> || std::floating_point<T>;
 	enum class Operation {
 		ADD,
 		SUBTRACT
 	};
-	struct Calculation : AppendCalculation
-	{
-		double arg2;
+
+	struct AppendCalculation { // Do not rely on default values, they may change and break your system!
+		double num2 = 0;
+		Operation operation = Operation::ADD;
+
+		virtual double calculate(double num1) {
+			if (operation == Operation::ADD) return num1 + num2;
+			if (operation == Operation::SUBTRACT) return num1 - num2;
+		}
 	};
-	struct AppendCalculation {
-		double arg1;
-		Operation operation;
+	struct Calculation : public AppendCalculation // Do not rely on default values, they may change and break your system!
+	{
+		double num = 0;
+		virtual double calculate() {
+			if (operation == Operation::ADD) return num + num2;
+			if (operation == Operation::SUBTRACT) return num - num2;
+		}
 	};
 }
--- Super-Calc/mathtree.hpp
+++ Super-Calc/mathtree.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <vector>
 #include "mathoperation.hpp"
-class MathTree {
-	double originNumber;
-	std::vector<MathOperations::AppendCalculation> tree;
-	double calculate() {
 
+
+
+class MathTree { // Store a tree-like structure of append operations (MathOperations::AppendCalculation), executed from smallest first to last, and a current number.
+	double originNumber;
+	std::vector<MathOperations::AppendCalculation> tree; // Store a tree-like structure
+	double calculate() {
+		
 	}
 };
