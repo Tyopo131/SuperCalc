a7a90494e1e0262c58e9085249418c964ed78336
Tyopo131 <samecob62@gmail.com>
2025-02-19 15:30:21 +0000

    Add tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#include <editor.hpp>
+#include <iostream>
+
 int main() {
-	
+#ifdef __APPLE__
+	auto values = editor::launch_specified("open -a TextEdit", "temp_file").get();
+	std::cout << values.first << std::endl << values.second;
+#elif _WIN32
+	auto values = editor::launch_specified("notepad", "temp_file").get();
+	std::cout << values.first << std::endl << values.second;
+#elif __linux__
+	auto values = editor::launch_specified("nano", "temp_file").get();
+	std::cout << values.first << std::endl << values.second;
+#endif
 }
